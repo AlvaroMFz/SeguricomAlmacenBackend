779aa96e0772a2ef25fc289d319f668d597fe3dd
Alvaro Fernandez <alvarofernandezmarcatoma@gmail.com>
2023-06-04 23:44:00 -0500

    Revert "Merge branch 'main' into carlos"
    This reverts commit dfc4d9291e5e61a94e6168e35a2f07d99481911c, reversing
    changes made to 9aa03a9251b73af756d938d9d36abd3a5925c893.

--- controllers/venta.controller.js
+++ controllers/venta.controller.js
@@ -1,73 +1,28 @@
 
-var Venta = require('../models/venta');
-var DetalleVenta = require('../models/detalleVenta');
-var Producto = require('../models/producto');
 
-function registrar(req, res) {
 
-    let data = req.body;
-    var venta = new Venta();
-    venta.idcliente = data.idcliente;
-    venta.iduser = data.iduser;
 
-    venta.save((err, venta_save) => {
-        if (venta_save) {
-            let detalles = data.detalles;
+function datos_venta(req,res){
+    var id = req.params['id'];
 
-            detalles.forEach((element, index) => {
-                var detalleVenta = new DetalleVenta();
-                detalleVenta.idproducto = element.idproducto;
-                detalleVenta.cantidad = element.cantidad;
-                detalleVenta.venta = venta_save._id;
-
-                detalleVenta.save((err, detalle_save) => {
-                    if (detalle_save) {
-                        Producto.findById({ _id: element.idproducto }, (err, producto_data) => {
-                            if (producto_data) {
-                                Producto.findByIdAndUpdate({ _id: producto_data._id }, {
-                                    stock: parseInt(producto_data.stock) -
-                                        parseInt(element.cantidad)
-                                }, (err, producto_edit) => {
-                                    res.end();
-                                })
-                            } else {
-                                res.send('No se encontró el producto');
-                            }
-                        });
-                    } else {
-                        res.send('No se pudo registrar los datos');
-                    }
-                })
-            });
-        } else {
-            res.send('No se pudo registrar los datos');
+    Venta.findById(id, (err,data_venta)=>{
+        if(data_venta){
+            Detalle.find({idventa:id},(err, data_detalle)=>{
+                if(data_detalle){
+                    res.status(200).send(
+                        {
+                            venta: data_venta,
+                            detalles: data_detalle
+                        }
+                    )
+                }
+            })
         }
-    });
+    }) 
 
+}
 
-
-
-    function datos_venta(req, res) {
-        var id = req.params['id'];
-
-        Venta.findById(id, (err, data_venta) => {
-            if (data_venta) {
-                Detalle.find({ idventa: id }, (err, data_detalle) => {
-                    if (data_detalle) {
-                        res.status(200).send(
-                            {
-                                venta: data_venta,
-                                detalles: data_detalle
-                            }
-                        )
-                    }
-                })
-            }
-        })
-    }
-
-    module.exports = {
-        registrar,
-        datos_venta,
-    }
+module.exports = {
+    registrar,
+    datos_venta,
 }
--- models/detalleVenta.js
+++ models/detalleVenta.js
@@ -4,7 +4,6 @@ var Schema = mongoose.Schema;
 var DetalleVentaSchema = Schema({
     idproducto: {type: Schema.ObjectId, ref: 'producto'},
     cantidad: Number,
-    venta:{type:Schema.ObjectId, ref:'venta'}
 });
 
 module.exports = mongoose.model('detalleventa', DetalleVentaSchema);
